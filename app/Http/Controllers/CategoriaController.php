811e99d28b197d8cde6bec265a3bfdabbef1cb02
JonathanVincentBa <jonathanvincent@outlook.com>
2021-04-25 21:36:07 -0500

    Seguridad en las peticiones HTTP

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -8,14 +8,20 @@ use App\Models\Categoria;
 
 class CategoriaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        if (!$request->ajax()){
+            return redirect('/');
+        }
         $categorias = Categoria::all();
         return $categorias;
     }
 
     public function store(Request $request)
     {
+        if (!$request->ajax()){
+            return redirect('/');
+        }
         $categoria = new Categoria();
         $categoria->nombre = $request->nombre;
         $categoria->descripcion = $request->descripcion;
@@ -25,6 +31,9 @@ class CategoriaController extends Controller
 
     public function update(Request $request)
     {
+        if (!$request->ajax()){
+            return redirect('/');
+        }
         $categoria = Categoria::findOrFail($request->id);
         $categoria->nombre = $request->nombre;
         $categoria->descripcion = $request->descripcion;
@@ -34,6 +43,9 @@ class CategoriaController extends Controller
 
     public function activar(Request $request)
     {
+        if (!$request->ajax()){
+            return redirect('/');
+        }
         $categoria = Categoria::findOrFail($request->id);
         $categoria->condicion = "1";
         $categoria->save();
@@ -41,6 +53,9 @@ class CategoriaController extends Controller
 
     public function desactivar(Request $request)
     {
+        if (!$request->ajax()){
+            return redirect('/');
+        }
         $categoria = Categoria::findOrFail($request->id);
         $categoria->condicion = "0";
         $categoria->save();
